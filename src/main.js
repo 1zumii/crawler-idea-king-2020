65885b64a995d6af63615073825d4f9891550b47
lyq <524123601@qq.com>
2020-09-19 22:18:39 +0800

    modify README.md

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 const request = require('request')
 const cheerio = require('cheerio')
+const fs = require('fs')
+
 const targetOrigin = "https://www.idea-king.org"
 const mainListUrl = "/mobile/tpxs.php?from=timeline"
 
@@ -17,3 +19,25 @@ function getHrefList() {
         })
     })
 }
+
+function parseWorksInfo(url) {
+    return new Promise((resolve, reject) => {
+        request(targetOrigin + url, function (error, response, body) {
+            if (error === null && response.statusCode === 200) {
+                const _c = cheerio.load(body)
+                const mainContent = _c('#dowebok')
+                const childrenContent = mainContent.find('div')
+                const temp = childrenContent.map((index, element) => _c(element).attr('class')).get()
+                resolve(temp)
+            } else {
+                reject(response.statusCode)
+            }
+        })
+    })
+}
+
+getHrefList().then(value => {
+    parseWorksInfo(value[0]).then(value => {
+        console.log(value)
+    })
+})
