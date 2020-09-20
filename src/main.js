7ba2f80034b56a734bd61e219f774955021ab713
lyq <524123601@qq.com>
2020-09-20 20:42:26 +0800

    未完成：cheerElement.attr('style') === 'margin-top:10px;'

--- src/main.js
+++ src/main.js
@@ -35,18 +35,38 @@ function parseWorksInfo(url) {
                 // 作品编号
                 const wn = mainContent.find('.wtt').get(0)
                 const worksNumber = _c(wn).text().split('：')[1]
-                // 需要遍历处理的部分，children 找所有的孩子节点，而不是使用 find 找所有的后代
-                const childrenContent = mainContent.children('div')
-                childrenContent.each((index, element) => {
-                    if (_c(element).hasClass('wtt')) {
-                        console.log(index, 'wtt')
-                    } else if (_c(element).attr('style') === 'margin-top:10px;') {
-                        console.log(index, 'style')
-                    } else if (_c(element).hasClass('votett')) {
-                        console.log(index, 'votett')
-                    } else {
-                        console.log(index, element.name)
+
+                // 创建文件及文件夹
+                const currentRootPath = `../backupResults/${worksNumber}`
+                fs.emptyDir(currentRootPath, err => {
+                    if (err) {
+                        reject(err)
+                        return
                     }
+                    // 需要写入的内容
+                    let writeContent = `# ${projectName} \n`
+                    writeContent += `- **投票数**：${thumbCount}\n`
+                    // 需要遍历处理的部分，children 找所有的孩子节点，而不是使用 find 找所有的后代
+                    const childrenContent = mainContent.children('div')
+                    childrenContent.each((index, element) => {
+                        const cheerElement = _c(element)
+                        if (cheerElement.hasClass('wtt')) {
+                            const unorderedListItem = cheerElement.text().split('：')
+                            writeContent += `- **${unorderedListItem[0]}**：${unorderedListItem[1]}\n`
+                        } else if (cheerElement.attr('style') === 'margin-top:10px;') {
+                            const illustrationAlt = _c(cheerElement.children('p').get(0)).text()
+                            const paragraphContent = _c(cheerElement.children('p').get(1)).text()
+                            writeContent += `\n${paragraphContent}\n`
+                        } else if (cheerElement.hasClass('votett')) {
+                            const paragraphTitle2 = cheerElement.children('.tt').text()
+                            const paragraphContent = cheerElement.children('div').text()
+                            writeContent += `## ${paragraphTitle2}\n`
+                            writeContent += `\n${paragraphContent}\n`
+                        }
+                    })
+                    // 写入文件
+                    // fs.writeFile 直接打开文件默认是 w 模式，所以如果文件存在，该方法写入的内容会覆盖旧的文件内容。
+                    fs.writeFileSync(`${currentRootPath}/index.md`, writeContent)
                 })
 
                 resolve({ projectName, url, worksNumber })
